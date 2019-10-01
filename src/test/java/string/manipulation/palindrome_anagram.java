20828470125235ba322c5ce6ae83e8b6dffb0d0a
Zuber <Zuber.Nargund@walmart.com>
2019-10-01 16:50:20 +0530

    added string palindrome

--- src/test/java/string/manipulation/palindrome_anagram.java
+++ src/test/java/string/manipulation/palindrome_anagram.java
@@ -1,5 +1,29 @@
 package string.manipulation;
 
 public class palindrome_anagram {
+	
+	
+	public static boolean stringPalindrome_charAt(String arg){
+		int len = arg.length();
+		
+		for(int i=0; i< (len/2) ;i++){
+			
+			if(arg.charAt(i) != arg.charAt(len-1-i)){
+				
+				return false;
+			}		
+		}
+		return true;
+
+	}
+	
+	
+	public static void main(String[] args){
+		
+		
+		System.out.println(stringPalindrome_charAt("redividerdetartrated"));
+		
+	}
+	
 
 }
